2fab6d1f87c75aa9d92154d934ce7a7002eba251
Anastasiya <Tsybusova>
2021-11-24 20:03:23 +0300

    Created branch Lesson4

---
+++ run.py
@@ -0,0 +1,11 @@
+from anastazia_framework.main import Framework
+from urls import routes, fronts
+from wsgiref.simple_server import make_server
+
+
+# Создаем объект WSGI-приложения
+application = Framework(routes,fronts)
+
+with make_server('', 8080, application) as httpd:
+    print('Запуск на порте 8080...')
+    httpd.serve_forever()
---
+++ urls.py
@@ -0,0 +1,20 @@
+from datetime import date
+from views import Index, Guess, Puzzle
+
+
+# front controller
+def secret_front(request):
+    request['data'] = date.today()
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
+routes = {
+    '/': Index(),
+    '/guess/': Guess(),
+    '/puzzle/': Puzzle(),
+}
